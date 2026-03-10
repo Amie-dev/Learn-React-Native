0f048ea28eedd7662091d2ba8c601ea45ffa6f29
amie-dev <learn.amie01@gmail.com>
2026-03-10 13:02:47 +0530

    fix prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  // Print width where Prettier will wrap code
+  printWidth: 80,
+
+  // Number of spaces per indentation level
+  tabWidth: 2,
+
+  // Use spaces instead of tabs
+  useTabs: false,
+
+  // Print semicolons at the end of statements
+  semi: true,
+
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+
+  // Use single quotes in JSX
+  jsxSingleQuote: false,
+
+  // Add trailing commas wherever possible
+  trailingComma: 'all',
+
+  // Print spaces between brackets in object literals
+  bracketSpacing: true,
+
+  // Put the > of a multi-line JSX element at the end of the last line
+  bracketSameLine: false,
+
+  // Include parentheses around a sole arrow function parameter
+  arrowParens: 'avoid',
+
+  // How prose should be wrapped
+  proseWrap: 'preserve',
+
+  // HTML whitespace sensitivity
+  htmlWhitespaceSensitivity: 'css',
+
+  // Line ending style
+  endOfLine: 'lf',
+
+  // Quote object properties only when required
+  quoteProps: 'as-needed',
+
+  // Put each HTML/JSX attribute on its own line
+  singleAttributePerLine: false,
+
+  // Control formatting of embedded languages
+  embeddedLanguageFormatting: 'auto'
+};
