49f5c96a23110dc0e3e94a2c24497128a316d07d
amie-dev <learn.amie01@gmail.com>
2026-03-13 21:10:00 +0530

    common style

---
+++ Chapter/Chapter_04/Example/CommonStyleDemo.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+ export const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'black',
+    color:'white',
+    padding:25
+  },
+  containerText:{
+    color:"white",
+    backgroundColor:"green",
+    padding:25,
+
+  }
+})
