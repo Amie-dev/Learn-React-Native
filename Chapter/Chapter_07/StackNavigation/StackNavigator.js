e29a6cd0585f61b0f32c229424833ecf214fcd3a
amie-dev <learn.amie01@gmail.com>
2026-03-17 23:40:46 +0530

    Fix

--- Chapter/Chapter_07/StackNavigation/StackNavigator.js
+++ Chapter/Chapter_07/StackNavigation/StackNavigator.js
@@ -1,30 +1,65 @@
-
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen } from './Screen/HomeScreen';
 import { AboutScreen } from './Screen/About';
-
-import { NavigationContainer } from '@react-navigation/native';
-
-import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 export const StackNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
           options={{ title: 'Home Screen' }}
         />
+
         <Stack.Screen
           name="AboutScreen"
           component={AboutScreen}
-          options={{
-            title: 'About Screen',headerShown:false
+          options={{ title: 'About Screen' ,
+            headerShown:false
           }}
         />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+
+// import { HomeScreen } from './Screen/HomeScreen';
+// import { AboutScreen } from './Screen/About';
+
+
+// import {NavigationContainer} from "@react-navigation/native"
+
+// import {createStackNavigator} from "@react-navigation/stack"
+
+// const Stack=createStackNavigator()
+
+
+// export const StackNavigation=()=>{
+//   return(
+//     <NavigationContainer>
+
+// <Stack.Navigator
+
+// >
+//   <Stack.Screen  name="HomeScreen" component={HomeScreen} 
+//   options={{title:"Home Screen"}} />
+//   <Stack.Screen name='AboutScreen'  
+//   component={AboutScreen}
+//   options={{
+//     title:"About Screen"
+//   }}
+
+//   />
+
+// </Stack.Navigator>
+
+//     </NavigationContainer>
+//   )
+// }
---
+++ Project/Project00/App00.js
@@ -0,0 +1,5 @@
+import {ProjectNavigation} from './Navigator'
+
+export const App00=()=>{
+  return <ProjectNavigation/>
+}
